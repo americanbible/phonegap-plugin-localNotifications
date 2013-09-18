26425cd5d2f17d8560a05862e90de3adfd65477f
Janson Hartliep <janson@elevatorup.com>
2013-09-17 21:54:59 -0400

    Added some initial jsdoc.

--- www/phonegap/plugin/localNotification/localNotification.js
+++ www/phonegap/plugin/localNotification/localNotification.js
@@ -4,6 +4,7 @@
  * @copyright WizCorp Inc. [ Incorporated Wizards ] 2011
  * @file localNotification.js for PhoneGap
  *
+ * Refinements & updates: @author American Bible Society
  */
 
 (function(cordova) {
@@ -14,18 +15,44 @@
 
   var localNotification = {
 
+    /**
+     * Schedules a notification now.
+     *
+     * @param {Number|String} id - the notification identifier
+     * @param {Object} options - hash of options
+     *
+     * Options include:
+     *
+     *  @param {Number} seconds - # of seconds from now to schedule (TODO: refactor to take a {Date})
+     *  @param {Number} badge - number for the badge
+     *  @param {String} message - text of the alertBody
+     *  @param {String} repeat - repeat interval: /hourly|daily|weekly|monthly|yearly/
+     */
     add : function(id, options) {
       return cordova.exec(null, null, "LocalNotification", "addNotification", [id, options]);
     },
 
+    /**
+     * Cancels a single notification
+     * 
+     * @param {Number|String} id - notification identifier
+     */
     cancel : function(id) {
       return cordova.exec(null, null, "LocalNotification", "cancelNotification", [id]);
     },
 
+    /**
+     * Cancels all scheduled notifications.
+     */
     cancelAll : function() {
       return cordova.exec(null, null,"LocalNotification", "cancelAllNotifications", []);
     },
 
+    /**
+     * Schedules a notification when the application enters the background.
+     *
+     * @see add()
+     */
     queue : function(id, options) {
       return cordova.exec(null, null, "LocalNotification", "queueNotification", [id, options]);
     },
